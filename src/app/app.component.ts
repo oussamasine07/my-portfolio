a5a32072566a74fce05b8e9151eb4deea1923ca0
sine oussama <sineoussama@gmail.com>
2025-04-13 23:18:06 +0100

    add the router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -10,8 +10,9 @@ import { ContactComponent } from './contact/contact.component';
 @Component({
   selector: 'app-root',
   imports: [ 
-    NavbarComponent, HomeComponent, AboutComponent, 
-    PortfolioComponent, SkillsComponent, ContactComponent
+    RouterOutlet, RouterLink,
+     NavbarComponent, // HomeComponent, AboutComponent, 
+    // PortfolioComponent, SkillsComponent, ContactComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'portfolio', component: PortfolioComponent },
+    { path: 'contact', component: ContactComponent }
+];
